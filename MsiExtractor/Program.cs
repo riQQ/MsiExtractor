8af805ef76064dcb8d3bad13e6634d43c33ecf20
riQQ <riQQ@users.noreply.github.com>
2020-03-28 16:43:21 +0100

    Add console app to extract a file from a .msi file

---
+++ MsiExtractor/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using Microsoft.Deployment.WindowsInstaller.Package;
+using System;
+
+namespace MsiExtractor
+{
+	internal class Program
+	{
+		private static int Main(string[] args)
+		{
+			Console.WriteLine($"Args: {string.Join(" ", args)}");
+
+			try
+			{
+				return ExtractMsi(args);
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine($"Exception extracting msi{Environment.NewLine}{exception}");
+				return 1;
+			}
+		}
+
+		private static int ExtractMsi(string[] args)
+		{
+			const int expectedNumberOfArgs = 2;
+			if (args.Length != expectedNumberOfArgs)
+			{
+				Console.WriteLine($"Expected exactly {expectedNumberOfArgs} argument");
+				return 2;
+			}
+
+			string packagePath = args[0];
+			string fileToExtract = args[1];
+
+			Console.WriteLine($"Starting to extract {fileToExtract} from {packagePath}");
+
+			using (InstallPackage pkg = new InstallPackage(packagePath, DatabaseOpenMode.ReadOnly))
+			{
+				string workingDirectory = pkg.WorkingDirectory;
+				InstallPathMap files = pkg.Files;
+				if (!files.ContainsKey(fileToExtract))
+				{
+					Console.WriteLine($"Couldn't find {fileToExtract} in msi file");
+					return 3;
+				}
+				pkg.ExtractFiles(new string[] { fileToExtract });
+				Console.WriteLine($"Successfully extracted {fileToExtract} to {workingDirectory}");
+			}
+
+			return 0;
+		}
+	}
+}
